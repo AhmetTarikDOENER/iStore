44e02afbb0b3ebd135f0bfa9670d963fd776f833
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-08-04 14:32:37 +0300

    Eliminate redundant codes to create view controller and embed in navigation controller by adding a method called createNavigationController method.

--- iStore/RootViewController.swift
+++ iStore/RootViewController.swift
@@ -5,24 +5,20 @@ final class RootViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
-        let redVC = UIViewController()
-        redVC.view.backgroundColor = .systemBackground
-        redVC.navigationItem.title = "Apps"
-        let redNavVC = UINavigationController(rootViewController: redVC)
-        redNavVC.navigationBar.prefersLargeTitles = true
-        redNavVC.tabBarItem = .init(title: "Apps", image: UIImage(named: "apps"), selectedImage: nil)
-        
-        let blueVC = UIViewController()
-        blueVC.view.backgroundColor = .systemBackground
-        blueVC.navigationItem.title = "Search"
-        let blueNavVC = UINavigationController(rootViewController: blueVC)
-        blueNavVC.navigationBar.prefersLargeTitles = true
-        blueVC.tabBarItem = .init(title: "Search", image: UIImage(named: "search"), selectedImage: nil)
-        
         viewControllers = [
-            redNavVC,
-            blueNavVC,
+            createNavigationController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavigationController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavigationController(viewController: UIViewController(), title: "Search", imageName: "search")
         ]
+    }
+    
+    private func createNavigationController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        viewController.navigationItem.title = title
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.view.backgroundColor = .systemBackground
+        navigationController.tabBarItem = .init(title: title, image: UIImage(named: imageName), selectedImage: nil)
+        navigationController.navigationBar.prefersLargeTitles = true
+        
+        return navigationController
     }
 }
